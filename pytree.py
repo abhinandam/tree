2f999f593328601fb1553c0f54c48f44687264d7
Abhishikth Nandam <abhinandam@gmail.com>
2016-10-04 10:20:51 -0400

    initial commit

--- pytree.py
+++ pytree.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+import os
 
 
-# YOUR CODE GOES here
+#def sortkeys(s):
+#  return re.sub('[^A-Za-z0-9]+', '', s).lower()
 
+
+def createTree(inputDirectory, indentLevel, numDirectories, numFiles):
+	files = os.listdir(inputDirectory)
+	for index, file in enumerate(files):
+		if (file[0] != "."):
+			nestedDirectory = inputDirectory + "/" + file
+			if (os.path.isfile(nestedDirectory)):
+				numFiles += 1
+				if (index == len(files) - 1):
+					print(indentLevel + '`-- ' + files[index])
+				else:
+					print(indentLevel + '|-- ' + file)
+			elif (os.path.isdir(nestedDirectory)):
+				numDirectories += 1
+				if (index == len(files) - 1):
+					print(indentLevel + '`-- ' + files[index])
+				else:
+					print (indentLevel + '|-- ' + file)
+				indentLevelTemp = indentLevel + '|   '
+				numDirectories, numFiles = createTree(nestedDirectory, indentLevelTemp, numDirectories, numFiles)
+
+	return numDirectories, numFiles
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+	inputDirectory = ""
+	if (len(sys.argv) == 1):
+		inputDirectory = "."
+	else:
+		inputDirectory = sys.argv[1]
+	print (inputDirectory)
+	numDirectories, numFiles = createTree(inputDirectory, "", 0, 0)
+	print ("\n" + str(numDirectories) + " directories, " + str(numFiles) + " files")
